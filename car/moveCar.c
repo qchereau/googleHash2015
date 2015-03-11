5fa171657e32a57a7b007ead964ed1694cce6668
CherAlain <cheralain@gmail.com>
2015-03-11 21:40:04 +0100

    ajout move

---
+++ car/moveCar.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+* deplacement d'une voiture                                               *
+*                                                                             *
+*******************************************************************************/
+
+/*-------------+
+|Include Files |
++-------------*/
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/*--------------------------------+
+| Variables globales au programme |
++--------------------------------*/
+
+#include "tab.h"
+
+
+int deplacement_suivant( long Its)
+{
+	/* Place dans tab */
+	long i, j,k;
+	i = Its*MoyRues;
+	if ( tab[i][0] < Its ) {
+	} 
+
+}
+
+
