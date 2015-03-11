b5dce3a434b5f8e76ceb6c8ad31724b657b3dc69
root <quentinche@hotmail.com>
2015-03-11 22:09:08 +0100

    première version direction voiture

--- car/moveCar.c
+++ car/moveCar.c
@@ -19,14 +19,43 @@
 #include "tab.h"
 
 
-int deplacement_suivant( long Its)
+long deplacement_suivant( long Its)
 {
-	/* Place dans tab */
-	long i, j,k;
-	i = Its*MoyRues;
-	if ( tab[i][0] < Its ) {
-	} 
-
+    int line;
+    int bestRatio, nextPosition;
+    /*on peut pas avoir de ratio negatif*/
+    bestRatio=-1;
+    line= founf_first_connexion(Its);
+    while(tab[line][0]==Its){
+        if(tab[line][5]>bestRatio){
+            bestRatio=tab[line][5];
+            nextPosition=tab[line][1];
+        }
+        line++;
+    }
+    
+    return nextPosition;
 }
 
+/*récupere le numéro de la première ligne de la node considérée*/
+int found_first_connexion (long node)
+{
+    	/* Place dans tab */
+	long i;
+	i = Its*MoyRues;
+        
+        /*cas element trop petit, il faut monté*/
+	if ( tab[i][0] < Its ) {
+            while(tab[i][0] < Its){
+                i++;
+            }
+            return i;
+	}else{
+            while(tab[i][0] >= Its){
+                i--;
+            }
+            return i+1;
+        }
+}
+}
 
