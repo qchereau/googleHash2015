196753356a626d43f47a7f77135baf49331b93d8
CherAlain <cheralain@gmail.com>
2015-03-11 19:48:07 +0100

    lecture fichier du deuxieme test

---
+++ car/tab.h
@@ -0,0 +1,30 @@
+/*
+pour tester 
+*/
+
+#ifndef EXT
+#define EXT extern
+#endif
+
+#define NB_COL 6
+#define NB_LIG 30000
+
+EXT long nC;
+EXT long nL;
+
+EXT long tab[NB_LIG][NB_COL];
+
+EXT long nCd;
+EXT long nLd;
+
+EXT double tabd[NB_LIG][NB_COL];
+
+
+EXT long nIntersec;
+EXT long nRoute;
+EXT long nCar;
+EXT long DureeMax;
+EXT long StartIntersec;
+
+EXT double MoyRues; 
+EXT long DureeSomme;
