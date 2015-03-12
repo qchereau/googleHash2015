81217ee4ff6b67e3b36850ef024365eed0f61a3d
root <quentinche@hotmail.com>
2015-03-12 20:53:29 +0100

    repartition entre groupe brutale

--- trialRound2015/repartition.c
+++ trialRound2015/repartition.c
@@ -18,67 +18,27 @@
 
 #include "tab.h"
 
-int servers_capacity[M];
-int servers_size[M];
+void repartir_serveur(){
+	int i,j;
+	int treatedGroup,minValue;
+	int group[NB_GROUP];
+	
+	memset(group,'\0',sizeof(group));
+	/*iteration sur tout les serveur*/
+	for(i=0; i<NB_SERV; i++){
+		/*determination du groupe le moins rempli*/
+		treatedGroup=0;
+		minValue=group[0];
+		for(j=1;j<NB_GROUP; j++){
+			if(group[j]<NB_GROUP){
+				treatedGroup=j;
+				minValue=group[j];
+			}
+		}
 
-int utility[M];
-int group_utility[P];
-
-
-int servers_order_by_utility_desc[M];
-Array groups[P]
-
-p = 0
-for(m=0; m<M; m++){
-    groups[p].push(m)
-    p = p + 1 mod P
+		serv[servSorted[i][1]][3]=treatedGroup;
+		group[treatedGroup] += servSorted[i][0];
+	}
 }
-for(p=0; p<P; p++){
-    group_utility[p] = 0;
-    for(m=0; m< groups[p].length; m++) {
-        group_utility[p] += group_utility[p] + utility[groups[p][m]];
-    }
-}
 
-# TODO: remplir le tableau
-int groups_order_by_group_utility_desc[P]
-
-group_id_max = groups_order_by_group_utility_desc[0]
-group_id_min = groups_order_by_group_utility_desc[-1]
-
-
-
-
-
-
-#######################################################################
-
-Soit M les serveurs à allouer sous forme m = (c, s, u) avec u utilité = c / s
-Soit P les groupes
-Soit Up l'utilité du groupe p
-M = sorted(M, key = u desc)
-p = P[0]
-for m in M:
-    ajouter m à p
-    p = suivant dans P
-for p in P:
-    Up = sum(u for m in p)
-soit pmax tq Upmax = (max Up for p in P)
-soit pmin tq Upmin = (min Up for p in P)
-
-soit m tq (u - (Upmax - Upmin)) minimal:
-    retirer m de pmax
-    ajouter m à pmin
-
-REPETER TANT QUE Upmax - Upmin est stable sur X itérations avec X le nombre de groupes
-
-soit pmax tq Spmax = (max sum(s for m in p) for p in P)
-
-for p in P par S croissante:
-    trouver les m à capacité égale entre p et pmax
-    echanger si la taille de celui de pmax est plus grande que celui de p et si la nouvelle taille de p reste plus petite que la nouvelle de pmax
-
-REPETER TANT QUE Spmax - Spmin est stable sur X itérations avec X le nombre de groupes
-
-A refaire avec des sommes de serveurs
 
